b45aca994b0bc318e900c45b2be24b9b292ef2e7
William Colin <williamcolin954@gmail.com>
2020-02-05 13:35:43 -0600

    Se agrega funcion para sacar el contacto del child_ids y se cambian los nombres de las variables

--- fields_addenda_walmart/models/models.py
+++ fields_addenda_walmart/models/models.py
@@ -5,4 +5,8 @@ from odoo import models, fields, api
 class AddFieldsAdendaWalmart(models.Model):
 	_inherit = 'res.partner'
 	
-	addres_contact_name = fields.Char(string="Child Contact Name")
+	child_contact_name1 = fields.Char(string="Child Contact Name")
+
+	def get_customer_contact_person(self):
+		if self.partner_id and self.partner_id.child_ids:
+			self.child_contact_name1 = ' '.join(str(person.name) for person in self.partner_id.child_ids)
