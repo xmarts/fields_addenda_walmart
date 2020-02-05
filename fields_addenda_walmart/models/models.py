6f1c0593baedeb055299da2ede3b49fe7d0e4636
William Colin <williamcolin954@gmail.com>
2020-02-05 13:38:21 -0600

    Se agrega el compute para jalar la funcion

--- fields_addenda_walmart/models/models.py
+++ fields_addenda_walmart/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class AddFieldsAdendaWalmart(models.Model):
 	_inherit = 'res.partner'
 	
-	child_contact_name1 = fields.Char(string="Child Contact Name")
+	child_contact_name1 = fields.Char(string="Child Contact Name", compute="get_customer_contact_person")
 
 	def get_customer_contact_person(self):
 		if self.partner_id and self.partner_id.child_ids:
