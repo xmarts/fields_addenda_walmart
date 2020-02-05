de2dc508e151a6b7967488d34e53672a4a30fddb
William Colin <williamcolin954@gmail.com>
2020-02-05 16:32:19 -0600

    Se agrega campo para los ultimos 4 digitos de la cuenta bancaria

--- fields_addenda_walmart/models/models.py
+++ fields_addenda_walmart/models/models.py
@@ -3,10 +3,13 @@
 from odoo import models, fields, api
 
 class AddFieldsAdendaWalmart(models.Model):
-	_inherit = 'res.partner'
+	_inherit = 'account.invoice'
 	
-	child_contact_name1 = fields.Char(string="Child Contact Name", compute="get_customer_contact_person")
+	child_contact_name1 = fields.Char(string="Child Contact Name", compute='get_customer_contact_person')
 
 	def get_customer_contact_person(self):
 		if self.partner_id and self.partner_id.child_ids:
-			self.child_contact_name1 = ' '.join(str(person.name) for person in self.partner_id.child_ids)
+			self.customer_contact = ' '.join(str(person.name) for person in self.partner_id.child_ids)
+
+
+	
