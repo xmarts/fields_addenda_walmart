25c964919477df478d166d159fe1fb4df6fa63b7
William Colin <williamcolin954@gmail.com>
2020-02-05 13:25:51 -0600

    .

--- fields_addenda_walmart/models/models.py
+++ fields_addenda_walmart/models/models.py
@@ -2,14 +2,7 @@
 
 from odoo import models, fields, api
 
-# class fields_addenda_walmart(models.Model):
-#     _name = 'fields_addenda_walmart.fields_addenda_walmart'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class AddFieldsAdendaWalmart(models.Model):
+	_inherit = 'res.partner'
+	
+	addres_contact_name = fields.Char(string="Child Contact Name")
